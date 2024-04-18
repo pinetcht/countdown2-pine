08f6a5f45bbdfe35df400c52488aadd302959438
Pine <pinetcht@gmail.com>
2024-04-18 23:29:26 +0200

    Correctly set correct/incorrect colors, replaced html elements, added correct answer counter

--- trivia-app/src/components/Trivia.jsx
+++ trivia-app/src/components/Trivia.jsx
@@ -5,8 +5,7 @@ import "../style/Trivia.css"
 
 const Trivia = () => {
   const [questions, setQuestion] = useState(null)
-  const [rightClick, setCorrectClick] = useState("white")
-  const [wrongClick, setIncorrectClick] = useState("white")
+  const [correctCount, setCorrectCount] = useState(0)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,29 +21,50 @@ const Trivia = () => {
     fetchData();
 
     fetchData().catch(console.error)
-  }, [])
+  }, []);
 
   return (
     <>
       <h1>Trivia App</h1>
+      <h3>Correct Answers: {correctCount}</h3>
       {questions && questions.map((question, index) => (
-        <>
-          <h2 key={index}>{question.question}</h2>
-            <div>
-                <button onClick={() => setCorrectClick("green")} style={{backgroundColor: rightClick}}>{question.correct_answer}</button>
-            </div>
-            {question.incorrect_answers.map((answer, i) => (
-              <div>
-                <button key={i} onClick={() => setIncorrectClick("pink")} style={{backgroundColor: wrongClick}}>{answer}</button>
-              </div>
-              ))}
-            
-        </>
-
+        <Question key={index} question={question} setCorrectCount={setCorrectCount} correctCount={correctCount}/>
       ))}
-
     </>
   );
 };
 
+const Question = ({ question, setCorrectCount, correctCount }) => {
+  const [rightClick, setCorrectClick] = useState("cream")
+  
+
+  return (
+    <>
+      <h2 className="question">
+        {question.question.replace(/&quot;/g, '"').replace(/&amp;/g, '&').replace(/&#039;/g, "'")}
+      </h2>
+      <div>
+        <button onClick={() => {setCorrectClick("DarkSeaGreen"); setCorrectCount(correctCount +1); }}style={{ backgroundColor: rightClick }} class="button">
+          {question.correct_answer}</button>
+      </div>
+      {question.incorrect_answers.map((answer, i) => (
+        <div>
+          <WrongAnswer key={i} answer={answer} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+const WrongAnswer = ({ answer }) => {
+  const [wrongClick, setIncorrectClick] = useState("cream")
+  return (
+    <button onClick={() => setIncorrectClick("pink")} style={{ backgroundColor: wrongClick }} className="button">
+      {answer.replace(/&quot;/g, '"').replace(/&amp;/g, '&').replace(/&#039;/g, "'")}
+    </button>
+  )
+ 
+}
+
 export default Trivia;
+
